a6082aa82f7687243cd9f5cc58ff83f42a96cf23
jokiwhite <jokiwhite@163.com>
2021-02-06 23:10:21 +0800

    bhd

--- src/pages/answer/index.vue
+++ src/pages/answer/index.vue
@@ -1,6 +1,24 @@
 <template> 
     <div>
-     <van-button type="primary" @click="jump">路由跳转</van-button>
+     <!-- <van-button type="primary" @click="jump">路由跳转</van-button> -->
+     <van-cell-group>
+      <van-field value="答题进度" input-align=center readonly />
+    </van-cell-group>
+     <van-progress :percentage="50" />
+
+
+    <van-form @submit="onSubmit">
+        <van-field
+  label="题目说明"
+  
+  autosize
+  type="textarea"
+  readonly 
+  value="请简要说明域名解析系统的工作过程（简述DNS提供的一些重要服务，以及整个DNS系统有哪几类域名服务器组成？）简述DNS应用系统的主要作用,系统组成，以及工作原理"
+/>
+    </van-form>
+
+
     </div>
     
 </template>
@@ -23,4 +41,6 @@ export default {
   }
 }
 </script>
+
+
 
